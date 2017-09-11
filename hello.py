46b2b741cd25106f0da799b0baf1e8ac58a2ca95
HAN <khaliltfan@163.com>
2017-09-11 21:02:37 +0800

    version 2b

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding:UTF-8 -*-
 from flask import Flask
+from flask.ext.script import Manager
 
 app = Flask(__name__)
+
+manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -13,4 +16,4 @@ def user(name):
     return '<h1>Hello, %s!</h1>' % name
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
