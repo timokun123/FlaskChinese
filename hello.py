eac9f2357dbd5a010970d2f8bbd26dca803e236a
HAN <khaliltfan@163.com>
2017-09-13 17:09:42 +0800

    version 4c

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:UTF-8 -*-
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
@@ -33,6 +33,9 @@ class NameForm(Form):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('貌似您更改过您的用户名')
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
